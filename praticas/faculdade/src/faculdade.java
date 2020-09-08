4143e35d35c38a3726c2613ba72e6c0c16d06637
nicholasscabral <nicholasscabral@gmail.com>
2020-09-08 19:42:46 -0300

    modify faculdade.java

--- praticas/faculdade/src/faculdade.java
+++ praticas/faculdade/src/faculdade.java
@@ -6,23 +6,32 @@ public class faculdade {
         Scanner input = new Scanner(System.in);
         String nome, area;
         int semestres;
+        boolean adicionar = true;
         ArrayList<Object> lista = new ArrayList<Object>();
 
         cursos curso = new cursos();
+        while (adicionar) {
+            System.out.println("nome do curso: ");
+            nome = input.next();
+            System.out.println("quantidade de semestres: ");
+            semestres = input.nextInt();
+            System.out.println("qual a area do curso: ");
+            area = input.next();
 
-        System.out.println("nome do curso: ");
-        nome = input.nextLine();
-        System.out.println("quantidade de semestres: ");
-        semestres = input.nextInt();
-        System.out.println("qual a area do curso: ");
-        area = input.next();
+            lista.add(curso.AddCurso(nome, semestres, area));
+            System.out.println("Deseja adicionar outro curso? true ou false?");
+            adicionar = input.nextBoolean();
+            if (!adicionar) {
+                adicionar = false;
+            }
+        }
 
-        //adicionando o curso
-        curso.AddCurso(nome, semestres, area);
-        System.out.println(curso);
+        for(int i = 0; i < lista.size(); i++) {
+            lista.set(i, String.valueOf(lista.get(i)));
+        }
 
-        //adicionando curso na lista de cursos
-        //lista.add(curso.AddCurso(nome, semestres, area));
-
+        for (Object i : lista) {
+            System.out.println(i);
+        }
     }
 }
