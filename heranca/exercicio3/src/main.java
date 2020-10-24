be9ec14765f1a7472436c57526830efad4024deb
nicholasscabral <nicholasscabral@gmail.com>
2020-10-24 00:26:25 -0300

    finishing exercicio3

--- heranca/exercicio3/src/main.java
+++ heranca/exercicio3/src/main.java
@@ -1,6 +1,30 @@
 public class main {
     public static void main(String[] args) {
-        
+        Fornecedor fornecedor = new Fornecedor();
+        Empregado empregado = new Empregado();
+        Administrador administrador = new Administrador();
+        Operario operario = new Operario();
+        Vendedor vendedor = new Vendedor();
+
+        // teste classe empregado
+        empregado.setImposto(0.10);
+        empregado.setSalarioBase(700.00);
+        double salarioEmpregado = empregado.calcularSalarioEmpregado();
+        System.out.println(salarioEmpregado);
+
+        // teste classe administrador
+        administrador.setAjudaDeCusto(400.00);
+        System.out.println(administrador.calcularSalarioAdministrador(salarioEmpregado));
+
+        // teste classe operario
+        operario.setComissao(0.20);
+        operario.setValorProducao(50.00);
+        System.out.println(operario.calcularSalarioOperario(salarioEmpregado));
+
+        // teste classe vendedor
+        vendedor.setValorVendas(300.00);
+        vendedor.setComissao(0.10);
+        System.out.println(vendedor.calcularSalarioVendedor(salarioEmpregado));
 
     }
 }
